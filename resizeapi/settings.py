4de6fcb955abf0d16de20ac990ce989ca716db71
ParthSharmaT <parthsharmatanguriya@gmail.com>
2022-04-01 15:38:13 +0530

    ch(app):change in settings.py added static_root and static_url

--- resizeapi/settings.py
+++ resizeapi/settings.py
@@ -124,5 +124,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT='media'
-MEDIA_URL=os.path.join(BASE_DIR,'/media/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
