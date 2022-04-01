d6f968d2a3d8cd3b93f38da503e71b56d633f3bf
ParthSharmaT <parthsharmatanguriya@gmail.com>
2022-04-01 17:11:05 +0530

    ch(app): serializer field modified

--- api/serializers.py
+++ api/serializers.py
@@ -4,5 +4,4 @@ from .models import FishRecord
 class FishRecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = FishRecord
-        fields = ('title', 'image', 'weight', 'length', 'created_at', 'updated_at')
-        
+        fields ='__all__'
