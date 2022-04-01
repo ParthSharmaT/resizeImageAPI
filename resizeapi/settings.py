e8c7af16008a1306ac14ed275f1a84c0d3ddabb1
ParthSharmaT <parthsharmatanguriya@gmail.com>
2022-04-01 15:43:10 +0530

    ch(app):changes in allowed hosts

--- resizeapi/settings.py
+++ resizeapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hj(^cg=iapz2=nlx&(ynar$!mn5c7#q()wx9&kqx3xf9eitpe_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['resizepic.herokuapp.com','127.0.0.1:8000']
+ALLOWED_HOSTS = ['resizepic.herokuapp.com','127.0.0.1']
 
 
 # Application definition
